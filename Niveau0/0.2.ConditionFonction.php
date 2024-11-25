30b61df4191515b25eb3ecd60bab672b44541886
QuochuyT <tran.github@gmail.com>
2024-11-25 12:15:59 +0100

    finish Condition

---
+++ Niveau0/0.2.ConditionFonction.php
@@ -0,0 +1,40 @@
+<?php
+
+function mineur($age) {
+
+    if (!is_numeric($age) || $age < 0) {
+    } if ($age <= 18) {
+        return "Mineur";
+    } else {
+        return "Majeur";
+    }
+}
+
+$age = 18;
+echo mineur($age);
+
+?>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="name">Prénom</label>
+        <input type="text" name="name" required>
+        <button type="submit">Envoyez</button>
+    </form>
+    <?php 
+    if (!empty($_POST)) {
+        if (isset($_POST['name'])){
+            echo 'Bienvenue' . ($_POST['name']);
+        };  
+    }
+    ?>
+    
+
+</body>
+</html>
