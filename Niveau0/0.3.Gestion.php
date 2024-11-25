f97d10805adf4b35139cad7aab0b4284c95ea20a
QuochuyT <tran.github@gmail.com>
2024-11-25 12:32:40 +0100

    init gestion

---
+++ Niveau0/0.3.Gestion.php
@@ -0,0 +1,20 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des fichiers</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="name">Nom</label>
+        <input type="text" name="name" required>
+
+        <label for="age">Âge</label>
+        <input type="number" name="age" required>
+
+        <label for="email">Prénom</label>
+        <input type="email" name="email" required>
+        <button type="submit">Enregistrer</button>
+    </form>
+</body>
+</html>
